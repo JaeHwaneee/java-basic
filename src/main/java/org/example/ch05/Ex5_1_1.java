22201aaa585b3a278f041c47b7e850cbaa4eaf30
JaeHwaneee <rkdldk810@gmail.com>
2023-03-25 01:56:26 +0900

    code formatting

--- src/main/java/org/example/ch05/Ex5_1_1.java
+++ src/main/java/org/example/ch05/Ex5_1_1.java
@@ -4,17 +4,14 @@ public class Ex5_1_1 {
     public static void main(String[] args) {
         int[] arr = new int[9]; // 길이가 5인 int 배열
 
-        // TODO 두개의 반복문을 돌아라
-
         // 첫번째 반복문에서는 arr 에 값만 할당해라
-        for (int i = 0; i < arr.length; i++){
-            arr[i] = 30+i;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 30 + i;
         }
 
         // 두번째 반복문에서는 arr 에 있는 값을 출력해라
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.println("arr[" + i + "]=" + arr[i]);
         }
-
     }
 }
