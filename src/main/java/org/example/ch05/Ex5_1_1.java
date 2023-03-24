1c39e7586fdb3c7a24a4e21b48dc6c702eb111d7
JaeHwaneee <rkdldk810@gmail.com>
2023-03-25 01:48:29 +0900

    add ch5 ex5_1, 5_1_1 변수

---
+++ src/main/java/org/example/ch05/Ex5_1_1.java
@@ -0,0 +1,20 @@
+package org.example.ch05;
+
+public class Ex5_1_1 {
+    public static void main(String[] args) {
+        int[] arr = new int[9]; // 길이가 5인 int 배열
+
+        // TODO 두개의 반복문을 돌아라
+
+        // 첫번째 반복문에서는 arr 에 값만 할당해라
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = 30+i;
+        }
+
+        // 두번째 반복문에서는 arr 에 있는 값을 출력해라
+        for(int i = 0; i < arr.length; i++){
+            System.out.println("arr[" + i + "]=" + arr[i]);
+        }
+
+    }
+}
