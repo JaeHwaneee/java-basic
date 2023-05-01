3cd7e7d3b05ec4d11a6bad9df30944b9d5cbb545
JaeHwaneee <rkdldk810@gmail.com>
2023-05-01 20:32:10 +0900

    프로그래머스 - 문자열을 정수로 변환하기

--- src/main/java/org/example/programmers/Problem2.java
+++ src/main/java/org/example/programmers/Problem2.java
@@ -1,4 +1,20 @@
 package org.example.programmers;
 
+/**
+ * 프로그래머스 - 문자열을 정수로 변환하기
+ */
 public class Problem2 {
+
+    // n_str = "10"
+    public int solution(String n_str) {
+        return Integer.valueOf(n_str);
+    }
+
+    public static void main(String[] args) {
+        Problem2 problem2 = new Problem2();
+
+        int response = problem2.solution("10");
+
+        System.out.println(response);
+    }
 }
