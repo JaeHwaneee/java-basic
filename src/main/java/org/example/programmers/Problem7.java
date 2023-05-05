c330ef3a791a7576522752c9bdb9ea5f60a81d2c
JaeHwaneee <rkdldk810@gmail.com>
2023-05-05 17:42:15 +0900

    7.프로그래머스 - 대소문자 바꿔서 출력하기

---
+++ src/main/java/org/example/programmers/Problem7.java
@@ -0,0 +1,32 @@
+package org.example.programmers;
+
+import java.util.Scanner;
+
+/**
+ * 7.프로그래머스 - 대소문자 바꿔서 출력하기
+ */
+public class Problem7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+        String answer = "";
+        // "aBcDeFg"
+        for (int index = 0; index < input.length(); index++) {
+            // 1. 문자 한개를 가져온다.
+            char ch = input.charAt(index);
+
+            // 2. 문자가 대문자인지 판단한다.
+            if (Character.isUpperCase(ch)) {
+                // 2-1 문자가 대문자라면, 소문자로 변경한다.
+                answer += Character.toLowerCase(ch);
+            } else if (Character.isLowerCase(ch)) {
+                // 3. 문자가 소문인지 판단한다.
+                answer += Character.toUpperCase(ch);
+                // 3-1 문자가 소문자라면, 대문자로 변경한다.
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
