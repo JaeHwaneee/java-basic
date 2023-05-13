48298ba8b591d16dc8567477ecc716986fdbc791
JaeHwaneee <rkdldk810@gmail.com>
2023-05-13 15:37:59 +0900

    13. 프로그래머스 - 문자열 붙여서 출력하기

---
+++ src/main/java/org/example/programmers/Problem13.java
@@ -0,0 +1,16 @@
+package org.example.programmers;
+
+// 13. 프로그래머스 - 문자열 붙여서 출력하기
+// apple pen
+// applepen
+import java.util.Scanner;
+
+public class Problem13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a+b);
+    }
+}
