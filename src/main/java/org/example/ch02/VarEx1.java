6fc9837089c747f7dec04e8365b90762e3f2aab6
JaeHwaneee <rkdldk810@gmail.com>
2023-05-30 17:51:21 +0900

    자바의 정석 2-2: 변수의 선언과 저장

---
+++ src/main/java/org/example/ch02/VarEx1.java
@@ -0,0 +1,11 @@
+package org.example.ch02;
+// 자바의 정석 2-2: 변수의 선언과 저장
+public class VarEx1 {
+    public static void main(String[] args) {
+        int x = 4, y = 2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
