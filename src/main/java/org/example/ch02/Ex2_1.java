e820b73bb6b7d510c909a98096b3c6edd8bdbe9c
JaeHwaneee <rkdldk810@gmail.com>
2023-05-30 17:19:23 +0900

    자바의 정석 2_1: 문구출력, 덧셈뺄셈하기

---
+++ src/main/java/org/example/ch02/Ex2_1.java
@@ -0,0 +1,10 @@
+package org.example.ch02;
+// 자바의 정석 2_1: 문구출력, 덧셈뺄셈하기
+public class Ex2_1 {
+    public static void main(String[] args) {
+        System.out.println(3-5);
+        System.out.println(3+5);
+        System.out.println(3/5);
+        System.out.println(3*5);
+    }
+}
